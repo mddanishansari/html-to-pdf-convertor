8279c7d8629d5ba0efce79cb1e59545c9b3a18bf
MD Danish Ansari <ansarid567@gmail.com>
2021-06-18 19:53:23 +0530

    Created files to convert HTML to PDF

---
+++ pdf-generator/src/main/java/android/print/PdfPrinter.kt
@@ -0,0 +1,59 @@
+package android.print
+
+import android.os.Build
+import android.os.CancellationSignal
+import android.os.ParcelFileDescriptor
+import android.print.PrintDocumentAdapter.LayoutResultCallback
+import android.print.PrintDocumentAdapter.WriteResultCallback
+import androidx.annotation.RequiresApi
+import java.io.File
+import java.io.IOException
+
+class PdfPrinter(private val printAttributes: PrintAttributes) {
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    fun generate(
+        printAdapter: PrintDocumentAdapter,
+        path: File,
+        onPdfGenerated: (File) -> Unit
+    ) {
+
+        if (path.exists()) {
+            path.delete()
+        }
+        try {
+            printAdapter.onLayout(null, printAttributes, null, object : LayoutResultCallback() {
+                override fun onLayoutFinished(info: PrintDocumentInfo, changed: Boolean) {
+                    printAdapter.onWrite(
+                        arrayOf(PageRange.ALL_PAGES),
+                        getOutputFile(path),
+                        CancellationSignal(),
+                        object : WriteResultCallback() {
+                            override fun onWriteFinished(pages: Array<PageRange>) {
+                                super.onWriteFinished(pages)
+                                onPdfGenerated(path)
+                            }
+                        })
+                }
+            }, null)
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+    private fun getOutputFile(file: File): ParcelFileDescriptor? {
+        val fileDirectory = file.parentFile
+        if (fileDirectory != null && !fileDirectory.exists()) {
+            fileDirectory.mkdirs()
+        }
+        try {
+            file.createNewFile()
+            return ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_WRITE)
+        } catch (e: IOException) {
+            throw e
+        }
+    }
+
+    interface OnPdfGeneratedListener {
+        fun onPdfGenerated(pdfFilePath: File)
+    }
+}
---
+++ pdf-generator/src/main/java/org/nvest/html_to_pdf/HtmlToPdfConvertor.kt
@@ -0,0 +1,97 @@
+package org.nvest.html_to_pdf
+
+import android.content.Context
+import android.os.Build
+import android.print.PdfPrinter
+import android.print.PrintAttributes
+import android.print.PrintAttributes.Resolution
+import android.print.PrintDocumentAdapter
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import androidx.annotation.UiThread
+import java.io.File
+
+class HtmlToPdfConvertor(private val context: Context) {
+
+    private var baseUrl: String? = null
+
+    fun setBaseUrl(baseUrl: String) {
+        this.baseUrl = baseUrl
+    }
+
+    @UiThread
+    fun convert(
+        pdfLocation: File,
+        htmlString: String,
+        onPdfGenerationFailed: PdfGenerationFailedCallback? = null,
+        onPdfGenerated: PdfGeneratedCallback,
+    ) {
+
+        // maintain pdf generation status
+        var pdfGenerationStarted = false
+        try {
+
+            // create new webview
+            val pdfWebView = WebView(context)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                pdfWebView.settings.safeBrowsingEnabled = false
+            }
+
+            // job name
+            val jobName = Math.random().toString()
+
+            // generate pdf attributes and properties
+            val attributes = getPrintAttributes()
+
+            // generate print document adapter
+            val printAdapter = getPrintAdapter(pdfWebView, jobName)
+
+            pdfWebView.webChromeClient = object : WebChromeClient() {
+                override fun onProgressChanged(view: WebView, newProgress: Int) {
+                    super.onProgressChanged(view, newProgress)
+
+                    // some times progress provided by this method is wrong, that's why we are getting progress directly provided by WebView
+                    val progress = pdfWebView.progress
+
+
+                    // when page is fully loaded, start creating PDF
+                    if (progress == 100 && !pdfGenerationStarted) {
+
+                        // change the status of pdf generation
+                        pdfGenerationStarted = true
+
+                        // generate pdf
+                        val pdfPrinter = PdfPrinter(attributes)
+                        pdfPrinter.generate(printAdapter, pdfLocation, onPdfGenerated)
+                    }
+                }
+            }
+
+            // load html in WebView when it's setup is completed
+            pdfWebView.loadDataWithBaseURL(baseUrl, htmlString, "text/html", "utf-8", null)
+
+        } catch (e: Exception) {
+            onPdfGenerationFailed?.invoke(e)
+        }
+    }
+
+
+    private fun getPrintAdapter(pdfWebView: WebView, jobName: String): PrintDocumentAdapter {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            pdfWebView.createPrintDocumentAdapter(jobName)
+        } else {
+            pdfWebView.createPrintDocumentAdapter()
+        }
+    }
+
+    private fun getPrintAttributes(pdfPageSize: PrintAttributes.MediaSize = PrintAttributes.MediaSize.ISO_A4): PrintAttributes {
+        return PrintAttributes.Builder().apply {
+            setMediaSize(pdfPageSize)
+            setResolution(Resolution("pdf", Context.PRINT_SERVICE, 600, 600))
+            setMinMargins(PrintAttributes.Margins.NO_MARGINS)
+        }.build()
+    }
+}
+
+private typealias PdfGeneratedCallback = (File) -> Unit
+private typealias PdfGenerationFailedCallback = (Exception) -> Unit
